696e1180c4387f1b2eb235ddd75f31967f874e16
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-05 23:08:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,35 +7,45 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
   get '/registrations/signup' do
+    # Render sign-up form view
     erb :'/registrations/signup'
   end
 
   post '/registrations' do
-    
+    # create new user from params hash, sign in, redirect to user home page
+    @user = User.new(name: params['name'], email: params['email'], password: params['password'])
+    @user.save
+
+    session[:id] = @user[:id]
     redirect '/users/home'
   end
 
   get '/sessions/login' do
+    # render login form
     erb :'sessions/login'
   end
 
   post '/sessions' do
-    
+    # login post, match params to db user object, sign in
+    @user = User.find_by(email: params['email'], password: params['password'])
+    session[:id] = @user.id
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
-
+  get '/sessions/logout' do
+    # clear session hash, redirect to index
+    session.clear
     redirect '/'
   end
 
   get '/users/home' do
-   
+    # render user home page view
+    @user = User.find(session[:id])
     erb :'/users/home'
   end
 
