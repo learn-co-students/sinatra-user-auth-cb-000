12d27aab292f74ffae0bfa110ba0ab88be459867
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-20 05:50:08 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,5 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171220051235) do
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-  end
+
 end
