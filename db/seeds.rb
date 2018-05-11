2c163140420b390c6ed3241e8d8e8457cf2225f7
Alex Segers <a.segers.dev@gmail.com>
2018-05-11 18:27:20 +0000

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create({name: 'Ethan Lam', email: "etham.lam@examplesite.com", password: 'ripbrody2007'})
+User.create({name: "Crystal Watts", email: "princess.crystal.869@examplesite.com", password: 'TiaraTime24142'})
+User.create({name: 'Jason Sanchez', email: "jsan.villanueva@examplesite.com", password: "navyelite97002"})
+User.create({name: "Kim Cheung", email: "cheung.kim@examplesite.com", password: "Ut45GsjhdhlU242"})
+User.create({name: "Tiffany Wright", email: 't.r.wright@examplesite.com', password: "teeteehouse1*23"})
