670303bba253faac87e8024ad88cc40c736570b1
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-14 23:36:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,9 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
-    if @user.password == params["password"]
+
+    if @user != nil
+      session[:id] = @user.id
       redirect '/users/home'
     else
       redirect '/sessions/login'
