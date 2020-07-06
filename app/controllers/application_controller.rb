666acd0a84510cbf847f4bec878a06228f22aa74
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-07-06 19:34:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    session[:user_id] = @user.id
 
     redirect '/users/home'
   end
@@ -25,7 +27,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-
+     @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
     redirect '/users/home'
   end
 
@@ -35,7 +41,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
+    @user = User.find(session[:user_id])
     erb :'/users/home'
   end
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,15 @@ Bundler.require
 
 ENV['SINATRA_ENV'] ||= "development"
 
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/user_auth#{ENV['SINATRA_ENV']}.sqlite"
 )
+
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
